3631c33cb3fd07fb4d90f430f721a6809ec0d8fd
daryl <darylo1@hotmail.com>
2021-07-05 13:06:43 +0200

    test61

--- po4a-stats.sh
+++ po4a-stats.sh
@@ -3,7 +3,9 @@
 
 # Remove stats file before creating new one
 rm -f "translator-files/statistics.md"
-echo "|language| file | status |" >> translator-files/statistics.md
+
+# Create table header
+echo "|Language| File | Status |" >> translator-files/statistics.md
 echo "|--------|------|--------|" >> translator-files/statistics.md
 
 produce_stats () {
@@ -11,24 +13,23 @@ produce_stats () {
 	while IFS= read -r -d '' file ; do
 		basename="$(basename -s .md "$file")"
 
-		# Stats printed to translator-files/statistics.txt
+		# Stats printed to translator-files/statistics.md
 		echo -n "|**"$lang"**| **"$basename".po**|" >> translator-files/statistics.md
 		msgfmt --statistics "$PO_DIR/$lang/$basename".po &>> translator-files/statistics.md
-		#echo -n '|' >> translator-files/statistics.md
-		#echo '' >> translator-files/statistics.md
 
 	done <   <(find -L "$SRC_DIR" -name "*.md"  -print0)
-	echo "|**-----**|**-----**|**-----**|" >> translator-files/statistics.md
+
+	#separator between languages
+	echo "|**-----**|**--------------------**|**--------------------**|" >> translator-files/statistics.md
 }
 
 # Run produce_stats on each language folder
 while IFS= read -r -d '' dir ; do
 	lang=$(basename -s .md "$dir")
-	#echo "## "$lang":" >> translator-files/statistics.md
-	#echo '' >> translator-files/statistics.md
 	produce_stats "$lang"
 done <   <(find "$PO_DIR" -mindepth 1 -maxdepth 1 -type d -print0)
 
-rm -f *.mo
+
+#rm -f *.mo
 
 echo Statistics file created
--- wiki/en/Central-Servers.md
+++ wiki/en/Central-Servers.md
@@ -5,4 +5,4 @@ lang: "en"
 permalink: "/wiki/Central-Servers"
 category: "hidden"
 ---
-THIS IS A TEST 17
+THIS IS A TEST 18
